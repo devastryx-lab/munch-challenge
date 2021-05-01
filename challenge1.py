7b3c4887741b6521d6c6a109391b98b6d7a2b18a
Puneet Gupta <gupta.puneet0696@gmail.com>
2021-05-01 15:13:29 +0530

    Refactor the interface to transfer data from source - The base assumption that IDs are incremental and the ID of a child will always be greater than the ID of it's parent was completely wrong - Create a tree while parsing the data from source JSON file. Time complexity : O(n) - Load the data starting with root node, followed by recursively adding the family of the root. Time complexity O(n)
    Signed-off-by: Puneet Gupta <gupta.puneet0696@gmail.com>

--- challenge1.py
+++ challenge1.py
@@ -1,73 +1,94 @@
-## Your solution here.
 import json
 from json.decoder import JSONDecodeError
-from typing import Dict, Iterator, List
+from typing import List, Dict
 
 from api1 import API1
+
+
+class Node:
+    """
+    Class representing a Product node
+    """
+
+    def __init__(self, product_id):
+        self.id = product_id
+        self.children = list()
+        self.parent = None
+        self.name = None
+
+
+class Tree:
+    """
+    Class representing a multi tree of the product catalogue from source
+    """
+
+    def __init__(self):
+        self.nodes = dict()
+
+    def push(self, product: Dict) -> None:
+        """
+        Push product node into the tree at the correct position
+        Args:
+            product: details of the product
+        """
+        parent_id = product["parent_id"]
+        product_id = product["id"]
+        if product_id not in self.nodes:
+            self.nodes[product_id] = Node(product_id)
+        self.nodes[product_id].name = product["name"]
+        if parent_id:
+            if parent_id not in self.nodes:
+                self.nodes[parent_id] = Node(parent_id)
+            self.nodes[product_id].parent = self.nodes[parent_id]
+            self.nodes[parent_id].children.append(self.nodes[product_id])
+
+    @property
+    def root_nodes(self) -> List[Node]:
+        """
+        List all root nodes
+
+        Returns:
+            list of all root nodes
+        """
+        return list(node for node in self.nodes.values() if node.parent is None)
 
 
 class TransferProductCatalogue:
     def __init__(self, source_file_name: str) -> None:
         self.source_file_name = source_file_name
-        self.cache = dict()  # this mimics an actual cache store
-        self.ancestors_dp = dict()
+        self.tree = Tree()
         self.api_client = API1()
 
-    def read_source_file(self) -> Iterator[Dict]:
+    def read_source_file(self) -> None:
         """
-        Read source file and yield products
-        Returns:
-            Yields product info form source
+        Read source file and create a Tree from the data
         """
         try:
             with open(self.source_file_name, "rb") as f:
-                # assuming that IDs are auto incremental values, which implies that a child ID will always be
-                # greater than the ID of it's ancestors
-                data = sorted(json.load(f), key=lambda x: x["id"])
+                data = json.load(f)
         except JSONDecodeError:
             raise ValueError("The input source file does not have a valid JSON")
         else:
             for row in data:
-                yield row
+                self.tree.push(row)
 
-    def find_ancestors(self, parent_id: int, ancestors: List) -> None:
+    def create_node_in_datastore(self, node: Node, ancestors: List) -> None:
         """
-        Find the ancestors of a product as follows:
-        * Try to fetch the ancestors of the immediate parent from ancestors_dp, if it has already been
-            computed
-        * Else, recursively find the ancestors of the parent
-
-        Append immediate parent name to the final list
-
+        Create node in the datastore and add all its children
         Args:
-            parent_id: ID of the parent product
+            node: Node to be inserted in the database
             ancestors: list of ancestors
         """
-        # every product that has children is saved in cache
-        parent_product_info = self.cache.get(parent_id)
-        ancestors.append(parent_product_info["name"])
-        if parent_of_parent := parent_product_info.get("parent_id"):
-            # check if the ancestors of parent have previously been computed
-            if ancestors_of_parent := self.ancestors_dp.get(parent_id):
-                ancestors.extend(ancestors_of_parent)
-            else:
-                ancestors_of_parent = list()
-                self.find_ancestors(parent_of_parent, ancestors_of_parent)
-                self.ancestors_dp[parent_id] = ancestors_of_parent
-                ancestors.extend(ancestors_of_parent)
+        parent_id = node.parent.id if node.parent else None
+        self.api_client.create({"name": node.name, "parent_id": parent_id, "ancestors": ancestors})
+        ancestors.append(node.name)
+        for child in node.children:
+            self.create_node_in_datastore(child, ancestors)
 
-    def load_data_from_external_source(self):
+    def load_data_from_external_source(self) -> None:
         """
         Load the data from external source into our system using API1
         """
-        for product in self.read_source_file():
-            # cache the product if it has any children
-            if product.pop("children_ids", None):
-                self.cache[product["id"]] = product
-            # find ancestors
-            ancestors = list()
-            if parent_id := product.get("parent_id"):
-                self.find_ancestors(parent_id, ancestors)
-            product["ancestors"] = ancestors
-            # create
-            self.api_client.create(product)
+        self.read_source_file()
+        for root in self.tree.root_nodes:
+            self.create_node_in_datastore(root, list())
